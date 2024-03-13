288dd00e96b41472a7a1a1361099f379f33bf6f7
Raj <rajkumarshahu@gmail.com>
2024-03-13 08:37:31 -0400

    Rename connectDB to DatabaseService

--- backend/src/api/database/DatabaseService.js
+++ backend/src/api/database/DatabaseService.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const DatabaseService = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log(
@@ -12,4 +12,4 @@ const connectDB = async () => {
 	}
 };
 
-module.exports = connectDB;
+module.exports = DatabaseService;
